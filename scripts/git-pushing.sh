d4b0800bc3f6f56d97f8520e4ced178a8472a789
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2024-10-24 04:30:27 -0600

    Update git-pushing.sh

--- scripts/git-pushing.sh
+++ scripts/git-pushing.sh
@@ -1,24 +1,67 @@
 #!/bin/bash
 
+# Functions
+get_keyvalue_timestamp() {
+    echo "$(date + "%Y-%m-%d_%H-%M-%S")"
+}
+
+log_event() {
+    local level="$1"
+    local event="$2"
+    local branch="$3"
+    local message="$4"
+
+    case "$level" in
+        "INFO") color="Green";;
+        "ERROR") color="Red";;
+        "WARNING") color="Yellow";;
+        *) color="White";;
+    esac
+
+    local timestamp
+
+    timestamp=$(get_keyvalue_timestamp)
+
+    echo "timestamp=$timestamp level=$level event=$event branch=$branch message='$message'"
+}
+
 # Variables
-BRANCH_NAME=$(git symbolic-ref --short HEAD)
+start_time=$(date +%s)
+branch_name=$(git symbolic-ref --short HEAD)
+start_time_readable=$(get_keyvalue_timestamp)
 
-# Push changes to current branch
-read -p "Enter commit message: " COMMIT_MESSAGE
+# Git operations
+log_event "INFO" "GitOperationStart" "$branch_name" "Operation started"
 
-if [ -z "$COMMIT_MESSAGE" ]; then
-    echo "Error: Commit message is required."
+read -p "Please enter commit message: " commit_message
+
+if [ -z "$commit_message" ]; then
+    log_event "ERROR" "GitCommitError" "$branch_name" "Commit message not provided"
     exit 1
 fi
+
+log_event "INFO" "GitAdd" "$branch_name" "Adding changes"
 
 git add .
-git commit -m "${COMMIT_MESSAGE}"
-git push origin "$BRANCH_NAME"
 
-# Check if the push was successful
+log_event "INFO" "GitCommit" "$branch_name" "Committing changes"
+
+git commit -m "${COMMIT_MESSAGE}"
+
+log_event "INFO" "GitPush" "$branch_name" "Pushing changes"
+
+git push origin "$branch_name"
+
 if [ $? -ne 0 ]; then
-    echo "Error while pushing to branch: ${BRANCH_NAME}"
+    log_event "ERROR" "GitPushError" "$branch_name" "Failed to push changes"
     exit 1
 else
-    echo "Changes pushed successfully to branch: ${BRANCH_NAME}"
+    log_event "INFO" "GitPushSuccess" "$branch_name" "Successfully pushed changes"
 fi
+
+log_event "INFO" "GitPushSuccess" "$branch_name" "Operation ended"
+
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+
+log_event "INFO" "GitOperationDuration" "$branch_name" "Processing time: $duration seconds."
