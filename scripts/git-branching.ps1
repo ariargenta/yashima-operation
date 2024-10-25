9c49efdd1592a2a465dbe65add6dca1add705c43
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2024-10-24 23:57:22 -0600

    Fix git merge comprobation

--- scripts/git-branching.ps1
+++ scripts/git-branching.ps1
@@ -22,22 +22,39 @@ if ([string]::IsNullOrEmpty($branchExists)) {
 
         git checkout -b $branch main
 
+        Write-Host "Branch $branch created locally. Pushing to remote..." -ForegroundColor Green
+
+        . "PSScriptRoot/git-pushing.ps1"
+
     } else {
         Log-Event -event "GitBranchCreate" -message "Branch $branch found remotely. Creating branch locally."
         
         git fetch origin
 
         git checkout -b $branch origin/$branch
+
+        Log-Event -event "GitPull" -message "Pulling latest changes from $branch."
+
+        git pull origin $branch
     }
 } else {
     Log-Event -event "GitCheckout" -message "Switching to branch $branch."
 
     git checkout $branch
-}
 
-Log-Event -event "GitPull" -message "Pulling latest changes from $branch."
+    $remoteBranchExists = git ls-remote --heads origin $branch
 
-git pull origin $branch
+    if ([string]::IsNullOrEmpty($remoteBranchExists)) {
+        Write-Host "Branch $branch exists locally but not remotely. Pushing the branch." -ForegroundColor Yellow
+
+        . "PSScriptRoot/git-pushing.ps1"
+
+        } else {
+            Log-Event -event "GitPull" -message "Pulling latest changes from $branch."
+
+            git pull origin $branch
+        }
+    }
 
 Log-Event -event "GitOperationEnd" -message "Branch switch completed."
 
